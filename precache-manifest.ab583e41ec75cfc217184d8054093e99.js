93d2dac3db11e6da8833df9c8e8de9b3f568d142
hananht <hananht@gmail.com>
2020-04-06 21:46:13 +0300

    Updates

---
+++ precache-manifest.ab583e41ec75cfc217184d8054093e99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50437986c217f9cef402eab990ea36f3",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "372a2cce1221cc25b9a1",
+    "url": "/monsters-rolodex/static/css/main.edf6744f.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "372a2cce1221cc25b9a1",
+    "url": "/monsters-rolodex/static/js/main.c65a16b9.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
